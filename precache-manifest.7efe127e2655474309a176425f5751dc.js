904345984ea97d64d4d2b8e9a830b9938eb01f70
anhlin <anhlin@ucsc.edu>
2019-09-13 03:32:00 -0700

    Updates

---
+++ precache-manifest.7efe127e2655474309a176425f5751dc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d2c35f078f5581e27673c446f55634",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b129caea45b11366d286",
+    "url": "/portfolio/static/css/main.b5c5e073.chunk.css"
+  },
+  {
+    "revision": "ebdb181b49ca1838a758",
+    "url": "/portfolio/static/js/2.8b4eb619.chunk.js"
+  },
+  {
+    "revision": "b129caea45b11366d286",
+    "url": "/portfolio/static/js/main.57956c29.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "56be615bbca4502de5d55d721dae917f",
+    "url": "/portfolio/static/media/bootstrap.56be615b.png"
+  },
+  {
+    "revision": "343ab03a15671f4a3e381f73d96f1aeb",
+    "url": "/portfolio/static/media/colors.343ab03a.jpg"
+  },
+  {
+    "revision": "54500fe3b0f75b3e6841a672247e156e",
+    "url": "/portfolio/static/media/contacts.54500fe3.png"
+  },
+  {
+    "revision": "de98c26eb9a8158fe7a54b6963fb0f20",
+    "url": "/portfolio/static/media/database.de98c26e.svg"
+  },
+  {
+    "revision": "98ff4822d03ab0f5746653f56441d38e",
+    "url": "/portfolio/static/media/express.98ff4822.png"
+  },
+  {
+    "revision": "1a19a7fdf806c43afe1e699d53c5f94f",
+    "url": "/portfolio/static/media/fe.1a19a7fd.jpg"
+  },
+  {
+    "revision": "74a6a0b732df55a2f0cc921891c63144",
+    "url": "/portfolio/static/media/freebsd.74a6a0b7.svg"
+  },
+  {
+    "revision": "daeaaf51189d49c32411056c83360f41",
+    "url": "/portfolio/static/media/mongodb.daeaaf51.png"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/portfolio/static/media/nodejslogo.6dcf6999.png"
+  },
+  {
+    "revision": "94fcb258f0da3f8b540e33238cf810c2",
+    "url": "/portfolio/static/media/profile.94fcb258.png"
+  },
+  {
+    "revision": "067050f6ecc418ea7a7d2f3557683c8c",
+    "url": "/portfolio/static/media/python.067050f6.png"
+  },
+  {
+    "revision": "1b0a459c71e9d7566e60f73869a366e9",
+    "url": "/portfolio/static/media/react-logo.1b0a459c.jpg"
+  },
+  {
+    "revision": "321aa57954025049b3a40a5ed733d4f5",
+    "url": "/portfolio/static/media/twitter.321aa579.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4379e4b57c4f77f3e30994dc0c329e38.js"
+  "/portfolio/precache-manifest.7efe127e2655474309a176425f5751dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
