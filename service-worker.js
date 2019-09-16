a82e1232b29cb57a1855c4f3995dda19592e7bba
anhlin <anhlin@ucsc.edu>
2019-09-16 13:38:22 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2cb96c1b873fd3428863f575acc5c7d6.js"
+  "/portfolio/precache-manifest.6bca87e232b91f374d5315fa7ad7f38b.js"
 );
 
 self.addEventListener('message', (event) => {
